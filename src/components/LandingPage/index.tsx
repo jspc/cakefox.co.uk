4eb467138ead7c53d5eef1410fef9b7ba10a219f
jspc <james+git@zero-internet.org.uk>
2022-09-03 16:43:06 +0100

    Split out landingpage logic

---
+++ src/components/LandingPage/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+//import hero from './hero.jpg';
+import logo from './logo.jpg';
+import style from './LandingPage.module.css';
+
+import { SocialIcon } from 'react-social-icons';
+
+export default function App() {
+    return (
+        <>
+            <img src={logo} alt="Cake Fox Logo" className="App-logo" />
+
+            <div className={style.socials}>
+                <SocialIcon url="https://www.facebook.com/CakeFoxBakes/" target="_blank" />
+                <SocialIcon url="https://www.instagram.com/cakefoxbakes" target="_blank" />
+                <SocialIcon url="mailto:penny@cakefox.co.uk" target="_blank" />
+            </div>
+        </>
+    );
+}
